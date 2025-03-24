c4ec404b04c398135a8a17f03ccc1fe91a6edf43
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-25 06:14:23 +0700

    create view to handling 404 not found

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <img src="../assets/images/notfound.jpg" alt="404" class="w-1/2" />
+    <h1 class="text-4xl font-bold text-gray-800 mb-4">Page Not Found</h1>
+    <router-link to="/" class="px-3 py-1 text-white rounded-lg bg-primary-100 hover:bg-primary-80">
+      Go to Home
+    </router-link>
+  </div>
+</template>
