b871d04e83c8d25fc0cc674e10035d34063bd5ba
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-25 06:14:09 +0700

    render card and section component in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,60 @@
 import NavBarComponent from '@/components/NavBarComponent.vue'
 import HeroComponent from '@/components/HeroComponent.vue'
 import SectionComponent from '@/components/SectionComponent.vue'
+import CardComponent from '@/components/CardComponent.vue'
+import { useServicesStore } from '@/stores/service'
+import { useLanguageStore } from '@/stores/language'
+import { watch } from 'vue'
+import { storeToRefs } from 'pinia'
+import CardSkeleton from '@/components/CardSkeletonComponent.vue'
+
+const servicesStore = useServicesStore()
+const languageStore = useLanguageStore()
+const { currentLanguage, currentTranslations } = storeToRefs(languageStore)
+
+watch(
+  currentLanguage,
+  (newLang) => {
+    servicesStore.fetchServices(newLang.toLowerCase())
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
   <main>
     <HeroComponent />
+
     <SectionComponent
-      title="What we Have"
-      subtitle="Our Services"
+      :title="currentTranslations.whatWeHave || 'What we Have'"
+      :subtitle="currentTranslations.services || 'Our Services'"
       :Image="true"
       imageSrc="/src/assets/images/vector.png"
       backgroundColor="bg-white"
       imagePosition="top-left"
-    />
+    >
+      <template v-if="servicesStore.loading">
+        <CardSkeleton />
+      </template>
+
+      <template v-else-if="servicesStore.error">
+        <div class="text-red-500 text-center py-8">Error: {{ servicesStore.error }}</div>
+      </template>
+
+      <template v-else-if="!servicesStore.hasServices">
+        <div class="text-center py-12 text-gray-500">No services available in this language</div>
+      </template>
+
+      <template v-else>
+        <CardComponent
+          v-for="service in servicesStore.services"
+          :key="service.id"
+          :title="service.title"
+          :description="service.description"
+          :iconSrc="service.iconSrc"
+          :link="service.link"
+        />
+      </template>
+    </SectionComponent>
   </main>
 </template>
