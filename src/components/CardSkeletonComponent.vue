ce97b817b8491e0231d05d0e482041362aaaaf3c
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-25 06:11:34 +0700

    create vue skeleton loading

---
+++ src/components/CardSkeletonComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card bg-white shadow-md rounded-lg overflow-hidden p-6">
+    <div class="animate-pulse">
+      <div class="h-12 w-12 bg-gray-200 rounded-full mb-4"></div>
+      <div class="h-6 bg-gray-200 rounded w-3/4 mb-2"></div>
+      <div class="h-4 bg-gray-200 rounded w-full mb-2"></div>
+      <div class="h-4 bg-gray-200 rounded w-5/6 mb-4"></div>
+      <div class="h-4 bg-gray-200 rounded w-1/3"></div>
+    </div>
+  </div>
+</template>
