fb9f64a2928d631a28f4425cb8308db19de6541b
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-24 17:45:20 +0700

    register fontawesome to main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUser, faHome)
+
 const app = createApp(App)
 
 app.use(createPinia())
