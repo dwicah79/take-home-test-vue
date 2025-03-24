4866b8b4b46b3ce53a4d3db8db2cf6e52a802ee8
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-24 20:48:31 +0700

    add state with pinia localization for ID and EN

---
+++ src/stores/language.js
@@ -0,0 +1,68 @@
+import { defineStore } from 'pinia'
+
+export const useLanguageStore = defineStore('language', {
+  state: () => ({
+    locale: localStorage.getItem('language') || 'en',
+    translations: {
+      en: {
+        services: 'Our Services',
+        itOutsourcing: 'IT Outsourcing',
+        independentStaff: 'Independent Staff Augmentation',
+        teamAugmentation: 'Team Augmentation',
+        itManagedServices: 'IT Managed Services Software Development',
+        digitalTransformation: 'Digital Transformation',
+        digitisation: 'Digitisation',
+        digitalisation: 'Digitalisation',
+        about: 'About',
+        aboutMgs: 'About MGS',
+        caseStudy: 'Case Study',
+        blog: 'Blog',
+        career: 'Career',
+        english: 'English',
+        indonesian: 'Bahasa Indonesia',
+        weAreHere: 'We Are Here',
+        itConsulting: 'IT Consultant',
+        outsourcing: 'Outsourcing',
+        techForBusiness:
+          'We believe that technology is for every business. From roadside stall business to publicly listed business. Elevate your business with technology.',
+        contactUs: 'Contact Us',
+        currentLanguage: 'EN',
+      },
+      id: {
+        services: 'Layanan Kami',
+        itOutsourcing: 'Outsourcing IT',
+        independentStaff: 'Peningkatan Staf Independen',
+        teamAugmentation: 'Peningkatan Tim',
+        itManagedServices: 'Layanan Manajemen IT dan Pengembangan Perangkat Lunak',
+        digitalTransformation: 'Transformasi Digital',
+        digitisation: 'Digitisasi',
+        digitalisation: 'Digitalisasi',
+        about: 'Tentang',
+        aboutMgs: 'Tentang MGS',
+        caseStudy: 'Studi Kasus',
+        blog: 'Blog',
+        career: 'Karir',
+        english: 'Inggris',
+        indonesian: 'Bahasa Indonesia',
+        weAreHere: 'Kami Hadir',
+        itConsulting: 'Konsultan IT',
+        outsourcing: 'Outsourcing',
+        techForBusiness:
+          'Kami percaya teknologi untuk semua bisnis. Dari usaha pinggir jalan hingga perusahaan publik. Tingkatkan bisnis Anda dengan teknologi.',
+        contactUs: 'Hubungi Kami',
+        currentLanguage: 'ID',
+      },
+    },
+  }),
+  actions: {
+    setLanguage(lang) {
+      this.locale = lang
+      localStorage.setItem('language', lang)
+      window.dispatchEvent(new Event('language-changed'))
+    },
+  },
+  getters: {
+    currentLanguage: (state) => state.locale.toUpperCase(),
+    currentTranslations: (state) => state.translations[state.locale],
+  },
+})
