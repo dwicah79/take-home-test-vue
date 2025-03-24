df0ec1f60e877156798760735f2bbabfc1609b3e
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-24 20:47:29 +0700

    register language store to main js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { useLanguageStore } from './stores/language'
+import './assets/main.css'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUser, faHome)
-
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+
+const languageStore = useLanguageStore()
+languageStore.setLanguage(localStorage.getItem('language') || 'en')
 
 app.mount('#app')
