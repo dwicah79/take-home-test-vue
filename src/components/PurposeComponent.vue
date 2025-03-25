fa064289ecf5242b4cb51f65468b6c70d7f8aab1
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-26 06:19:34 +0700

    create purpose component

---
+++ src/components/PurposeComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full py-10 md:py-32 bg-primary-100 relative overflow-hidden">
+    <div class="absolute top-0 right-0 opacity-40 md:opacity-100">
+      <img src="../assets/images/linebg.png" alt="line" class="w-full" />
+    </div>
+    <h1 class="text-center text-white font-semibold text-lg">Our Purpose</h1>
+    <h1 class="text-center text-white font-bold md:text-3xl text-xl">Technology Inclusivity</h1>
+  </div>
+</template>
